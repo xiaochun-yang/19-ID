3a4928820c9b114adb22e7980d69ebca14ff68ed
root <root@xf19id1-ws2.(none)>
2017-09-20 15:41:08 -0400

    add sudo filter working in simDhs

---
+++ dcss/scripts/devices/chutch_filter2.tcl
@@ -0,0 +1,72 @@
+# chutch_filter2.tcl
+
+
+proc chutch_filter2_initialize {} {
+	
+	# specify children devices
+	set_children white_beam_filter2
+}
+
+
+proc chutch_filter2_move { new_chutch_filter2 } {
+
+	# global variables
+	variable whte_beam_filter_1
+
+	# move white_beam_filter2
+	set new_position [chutch_filter2_calculate $new_chutch_filter2]
+	move white_beam_filter2 to $new_position
+
+	# wait for the moves to complete
+	wait_for_devices white_beam_filter2
+}
+
+
+proc chutch_filter2_set { new_chutch_filter2} {
+
+	# global variables
+	variable white_beam_filter2
+        set white_beam_filter2 [chutch_filter2_calculate $new_chutch_filter2]
+}
+
+
+proc chutch_filter2_update {} {
+
+	# global variables
+	variable white_beam_filter2
+        return [chutch_filter2_calculate1 $white_beam_filter2]
+}
+
+
+proc chutch_filter2_calculate { num } {
+
+	variable white_beam_filter2
+	if { $num == 5 } {
+                        return 16.23
+        } elseif { $num == 6 } {
+                        return -2.5
+        } elseif { $num == 7 } {
+                        return -20
+        } elseif { $num == 8 } {
+                        return -38.3
+        } else {
+                        return $white_beam_filter2
+        }
+}
+
+proc chutch_filter2_calculate1 { white_beam_filter2 } {
+        variable chutch_filter2
+        
+        if { [expr abs([expr $white_beam_filter2 - 16.23])] < 0.2 } {
+                return 5
+        } elseif { [expr abs([expr $white_beam_filter2 + 2.5])] < 0.2 } {
+                return 6
+        } elseif { [expr abs([expr $white_beam_filter2 + 20])] < 0.2 } {
+                return 7
+        } elseif { [expr abs([expr $white_beam_filter2 + 38.3])] < 0.2 } {
+                return 8
+        } else {
+                return $chutch_filter2
+        }
+}
+
