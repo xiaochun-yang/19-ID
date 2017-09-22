a92acd0e0ff7ba3d2fb12ecc5f43cef602baa55f
root <root@xf19id1-ws2.(none)>
2017-09-22 15:00:47 -0400

    sudo filter add in bluice

--- BluIceWidgets/WBControlView.tcl
+++ BluIceWidgets/WBControlView.tcl
@@ -28,8 +28,10 @@ class DCS::WBControlWidget {
 
  public proc getMotorList { } {
         return [list \
-        white_beam_filter_1 \
-        white_beam_filter_2 \
+        white_beam_filter1 \
+        white_beam_filter2 \
+#        white_beam_filter_1 \
+#        white_beam_filter_2 \
         white_beam_mask_x \
         white_beam_mask_z \
         white_beam_slit_upper \
@@ -42,8 +44,10 @@ class DCS::WBControlWidget {
                 place $itk_component(control) -x 150 -y 275
 
                 # construct the table widgets
-                motorView white_beam_filter_1 125  90 sw
-                motorView white_beam_filter_2 300 90 sw
+                motorView white_beam_filter1 125  90 sw
+                motorView white_beam_filter2 300 90 sw
+             #  motorView white_beam_filter_1 125  90 sw
+             #  motorView white_beam_filter_2 300 90 sw
                 motorView white_beam_mask_x 125 170 sw
                 motorView white_beam_mask_z 300 170 sw
                 motorView white_beam_slit_lower 125 250  sw
@@ -51,8 +55,29 @@ class DCS::WBControlWidget {
 
                 # draw the table
                 #rect_solid 180 120 250 20 40 60 40
-
-
+		itk_component add unit1 {
+                  # make the optimize beam button
+                  label $itk_component(canvas).unit1 \
+                      -text "Num" \
+                      -relief flat \
+                      -width 4 \
+              	} {
+                           keep -foreground
+              	}
+		itk_component add unit2 {
+                  # make the optimize beam button
+                  label $itk_component(canvas).unit2 \
+                      -text "Num" \
+                      -relief flat \
+                      -width 4 \
+              	} {
+                           keep -foreground
+              	}
+		place $itk_component(unit1) -x 220 -y 85 -anchor sw
+		place $itk_component(unit2) -x 395 -y 85 -anchor sw
+		
+              #        -background #c0c0ff \
+              #        -activebackground #c0c0ff
 
                 eval itk_initialize $args
                 $itk_component(canvas) configure -width 560 -height 310
---
+++ dcss/scripts/devices/white_beam_filter1.tcl
@@ -0,0 +1,71 @@
+# white_beam_filter1.tcl
+
+
+proc white_beam_filter1_initialize {} {
+	
+	# specify children devices
+	set_children white_beam_filter_1
+}
+
+
+proc white_beam_filter1_move { new_white_beam_filter1 } {
+
+	# global variables
+	variable whte_beam_filter_1
+
+	# move white_beam_filter_1
+	move white_beam_filter_1 to [white_beam_filter1_calculate $new_white_beam_filter1] 
+
+	# wait for the moves to complete
+	wait_for_devices white_beam_filter_1
+}
+
+
+proc white_beam_filter1_set { new_white_beam_filter1} {
+
+	# global variables
+	variable white_beam_filter_1
+        set white_beam_filter_1 [white_beam_filter1_calculate $new_white_beam_filter1]
+}
+
+
+proc white_beam_filter1_update {} {
+
+	# global variables
+	variable white_beam_filter_1
+        return [white_beam_filter1_calculate1 $white_beam_filter_1]
+}
+
+
+proc white_beam_filter1_calculate { num } {
+
+	variable white_beam_filter_1
+	if { $num == 1 } {
+                        return 19.2
+        } elseif { $num == 2 } {
+                        return 1
+        } elseif { $num == 3 } {
+                        return -17.5
+        } elseif { $num == 4 } {
+                        return -35.5
+        } else {
+                        return $white_beam_filter_1
+        }
+}
+
+proc white_beam_filter1_calculate1 { white_beam_filter_1 } {
+        variable white_beam_filter1
+        
+        if { [expr abs([expr $white_beam_filter_1 - 19.2])] < 0.2 } {
+                return 1
+        } elseif { [expr abs([expr $white_beam_filter_1 - 1])] < 0.2 } {
+                return 2
+        } elseif { [expr abs([expr $white_beam_filter_1 + 17.5])] < 0.2 } {
+                return 3
+        } elseif { [expr abs([expr $white_beam_filter_1 + 35.5])] < 0.2 } {
+                return 4
+        } else {
+                return $white_beam_filter1
+        }
+}
+
