829344c1d60b5ed4ff209b104ca02f3aa857e6ba
root <root@xf19id1-ws2.(none)>
2017-11-15 15:55:47 -0500

    fix the data collection overwrriten problem with more than 360 frames

--- pilatusDhs/scripts/PilatusControl.tcl
+++ pilatusDhs/scripts/PilatusControl.tcl
@@ -29,6 +29,7 @@
 set pilatusMessage -
 
 #yangx add
+set gdestinationDir ""
 #set i 0
 
 #puts with special frame header
@@ -406,7 +407,9 @@ puts "yangx tmpDir=$tmpDir"
     }
 
     private method externalTrigger { } {
-        configureHeader $_multiImageParams
+        global gdestinationDir
+
+	configureHeader $_multiImageParams
 
         #set the exposure time
         set nFrameImg [$_multiImageParams cget -nFrameImg ] 
@@ -438,38 +441,45 @@ puts "yangx tmpDir=$tmpDir"
 	set fileName [$_multiImageParams cget -filename]
         set startIndex [$_multiImageParams cget -startIndex]
 
-	if {$startIndex <= 360}{
-		#startIndex is not less than 361 means it's continue from 360. In
-		#this case, it not need to check the directory. Otherwise 
-		#check if the file name is already exist in the current directory.
-        	#should use imperson to deal with this. but
-		#haven't figour out how to do it yet
-		set ii 1
-		while {$ii} {
-			set fl [glob -nocomplain $destinationDir/$fileName*]
-#			puts "yangxx fl =$fl"
-			if {$fl != ""} {
-                		#make a new filename
-                		set destinationDir $destinationDir/OVERWRITTEN
-#				puts "yangx destinationDir =$destinationDir"
-        		} else {
-#				puts "yangx1 destinationDir =$destinationDir"
-				file mkdir $destinationDir
-                		set ii 0
-        		}
-		}
-	}
+	if {$startIndex <= 360} {
+                #startIndex is not less than 361 means it's continue from 360. In
+                #this case, it not need to check the directory. Otherwise 
+                #check if the file name is already exist in the current directory.
+                #should use imperson to deal with this. but
+                #haven't figour out how to do it yet
+                set ii 1
+                while {$ii} {
+                        set fl [glob -nocomplain $destinationDir/$fileName*]
+#                       puts "yangxx fl =$fl"
+                        if {$fl != ""} {
+                                #make a new filename
+                                set destinationDir $destinationDir/OVERWRITTEN
+#                               puts "yangx destinationDir =$destinationDir"
+                        } else {
+				set gdestinationDir $destinationDir
+#                                puts "yangx gdestinationDir =$gdestinationDir"
+                                file mkdir $destinationDir
+                                set ii 0
+                        }
+                }
+        }
+	# else {
+        #        puts "yangx else gdestinationDir =$gdestinationDir"
+        #	file mkdir $gdestinationDir
+        #}
+	
 	putsDet "filename $fileName"
-	putsDet "directory $destinationDir"
+	putsDet "directory $gdestinationDir"
+        putsDet "startIndex $startIndex"
+
 #puts "yangx filename=$fileName"
 #puts "yangx directory=$destinationDir"
-#       set startIndex [$_multiImageParams cget -startIndex]
-        putsDet "startIndex $startIndex"
+#puts "yangx startIndex=$startIndex"
 
         #setup up local directory to put the data set
         set localDir "${tmpDir}/$userName/"
         file mkdir $localDir
-#puts "localdir=$localDir"
+#puts "yangx localdir=$localDir"
         #setup unique file names for local files
         set uid [clock seconds]
         $_multiImageParams configure -localUniqueRunId $uid
