355aaee268a17dbf0e3796a78c80b5d820f4d306
root <root@xf19id1-ws2.(none)>
2018-01-23 17:46:27 -0500

    update camera PV scan

--- dcss/scripts/operations/cam3_op.tcl
+++ dcss/scripts/operations/cam3_op.tcl
@@ -3,9 +3,10 @@ proc cam3_op_initialize {} {
 
 proc cam3_op_start { time } {
 
-	variable cam3_Intensity
-#        after $time
+	variable cam3_stats1_max
+	set tm [expr $time*1000]
+	after [format "%5.0f" $tm]
 #        set camInt [$deviceFactory getObjectName $camIntensity]
 #        set camValue [$camInt getContents]
-        return $cam3_Intensity
+        return $cam3_stats1_max
 }
